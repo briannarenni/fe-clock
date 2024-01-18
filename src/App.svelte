8f571eb28db04f02c6dc5a11d354afe4adbf2109
Brianna Renni <briannaxrene@icloud.com>
2024-01-18 15:44:52 -0800

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+</script>
+
+<main>
+  <p>Good [time of day], it's currently</p>
+  <h1>[time]</h1>
+  <h3>in [location]</h3>
+  <button>More</button> <button>Less</button>
+  <p>Current timezone</p>
+  <p>Day of the year</p>
+  <p>Day of the week</p>
+  <p>Week number</p>
+</main>
+
+<style>
+</style>

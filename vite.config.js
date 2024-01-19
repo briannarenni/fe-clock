28ec26090e504cb835280d57db8424d3d9aae848
Brianna Renni <briannaxrene@icloud.com>
2024-01-19 15:51:53 -0800

    (chore): Config setup, add styles folder, install Ky, update README

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-})
+  server: {
+    open: true
+  },
+  base: '/fe-clock/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+      '@src': path.resolve(__dirname, './src'),
+      '@lib': path.resolve(__dirname, './src/lib'),
+      '@stores': path.resolve(__dirname, './src/stores'),
+      '@scripts': path.resolve(__dirname, './src/scripts'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@btn': path.resolve(__dirname, './src/lib/btn'),
+      '@components': path.resolve(__dirname, './src/lib/components'),
+      '@layout': path.resolve(__dirname, './src/lib/layout')
+    }
+  }
+});
